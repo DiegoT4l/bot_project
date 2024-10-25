a55b62b1b4e4c7801cd7eae4d89396b3a7d54936
DiegoT4l <lopez.diiego.107@hotmail.com>
2024-10-24 20:55:14 -0600

    feat(types): Add ValidationHandler interface and related types

---
+++ src/types/validation-handler.ts
@@ -0,0 +1,14 @@
+// deno-lint-ignore-file ban-types
+/**
+ * Validation handler options (validationsPath).
+ */
+export interface ValidationHandlerOptions {
+  validationsPath: string;
+}
+
+/**
+ * Private validation handler data.
+ */
+export interface ValidationHandlerData extends ValidationHandlerOptions {
+  validations: Function[];
+}
